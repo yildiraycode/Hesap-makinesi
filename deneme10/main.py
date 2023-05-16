8e1127486ac5cc897a025c9e5262c566f6760188
Yıldıray ALADAĞ <128586234+yildiraycode@users.noreply.github.com>
2023-05-16 15:40:52 +0300

    toplama ve bölme işlemi
    topla ve böl işlemi

---
+++ deneme10/main.py
@@ -0,0 +1,6 @@
+print("""*******HESAP MAKİNESİ******""")
+sayi1=float(input("Birinci sayiyi giriniz: "))
+sayi2=float(input("Ikinci sayiyi giriniz: "))
+print("""
+Toplam:{}
+Bolme:{}""".format(sayi1+sayi2,sayi1/sayi2))
